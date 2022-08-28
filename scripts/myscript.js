d1718fff44e17733b9f0bd0dc5bd8da9e6edffd0
Federico <federico@fastmail.com>
2022-08-28 16:53:30 +0200

    Major website update.
    - Cool background
    - Cool buttons
    - Cool text? idk

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -8,10 +8,8 @@
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.005}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.010}% ${50 - (_mouseY - _h) * 0.010}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.030}% ${50 - (_mouseY - _h) * 0.030}%`;
-        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.03}%`;
+        let x = `${_depth1}`;
         console.log(x);
         elem.style.backgroundPosition = x;
     }
