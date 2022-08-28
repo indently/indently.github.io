26c34ffff13a6b3089ab7d07daed64856400a00a
Federico <federico@fastmail.com>
2022-08-28 11:54:40 +0200

    Added a background effect and revised text

---
+++ scripts/myscript.js
@@ -0,0 +1,19 @@
+(function() {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.005}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.010}% ${50 - (_mouseY - _h) * 0.010}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.030}% ${50 - (_mouseY - _h) * 0.030}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
+
+})();
