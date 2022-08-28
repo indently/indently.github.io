b6140e0b5daf3ee3e8fed034570ffa5e7ddc181c
Federico <federico@fastmail.com>
2022-08-28 18:41:06 +0200

    Slowed down parallex effect

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -8,7 +8,7 @@
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.03}%`;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.005}%`;
         let x = `${_depth1}`;
         console.log(x);
         elem.style.backgroundPosition = x;
